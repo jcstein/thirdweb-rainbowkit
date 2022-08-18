32aed32da0b9253b1868976768a38cd9c68c6ea9
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2022-08-17 22:39:54 -0400

    gm gm

---
+++ pages/_app.tsx
@@ -0,0 +1,115 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+  useSigner,
+} from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import {
+  connectorsForWallets,
+  wallet,
+  RainbowKitProvider,
+  midnightTheme,
+} from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import { ThirdwebSDKProvider, ChainId } from "@thirdweb-dev/react";
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: true,
+};
+const fonts = {
+  heading: `"Inter", sans-serif`,
+  body: `"Inter", sans-serif`,
+};
+
+const theme = extendTheme({ config, fonts });
+
+const { chains, provider } = configureChains(
+  [chain.goerli],
+  [
+    jsonRpcProvider({
+      rpc: () => {
+        return {
+          http: "https://rpc.ankr.com/eth_goerli",
+        };
+      },
+    }),
+    publicProvider(),
+  ]
+);
+
+const connectors = connectorsForWallets([
+  {
+    groupName: "Recommended",
+    wallets: [
+      wallet.metaMask({ chains, shimDisconnect: true }),
+      wallet.walletConnect({ chains }),
+      wallet.coinbase({ appName: "probably nothing", chains }),
+      wallet.rainbow({ chains }),
+    ],
+  },
+  {
+    groupName: "Others",
+    wallets: [
+      wallet.argent({ chains }),
+      wallet.brave({
+        chains,
+        shimDisconnect: true,
+      }),
+      wallet.imToken({ chains }),
+      wallet.injected({
+        chains,
+        shimDisconnect: true,
+      }),
+      wallet.ledger({
+        chains,
+      }),
+      wallet.steak({ chains }),
+      wallet.trust({ chains, shimDisconnect: true }),
+    ],
+  },
+]);
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider,
+});
+
+const activeChainId = ChainId.Goerli;
+
+function ThirdwebProvider({ wagmiClient, children }: any) {
+  const { data: signer } = useSigner();
+  return (
+    <ThirdwebSDKProvider
+      desiredChainId={activeChainId}
+      signer={signer as any}
+      provider={wagmiClient.provider}
+      queryClient={wagmiClient.queryClient as any}
+    >
+      {children}
+    </ThirdwebSDKProvider>
+  );
+}
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains} theme={midnightTheme()} coolMode>
+          <ThirdwebProvider wagmiClient={wagmiClient}>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+            <Component {...pageProps} />
+          </ThirdwebProvider>
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </ChakraProvider>
+  );
+}
+export default MyApp;
